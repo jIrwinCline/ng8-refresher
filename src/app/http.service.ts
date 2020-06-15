8488d8c208e3809bdb2932aadd144889615b4d37
jirwincline <jci@pdx.edu>
2020-06-15 11:27:04 -0700

    add rvice and test method

---
+++ src/app/http.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class HttpService {
+  constructor() {}
+
+  myMethod() {
+    return console.log("hey, whats up");
+  }
+}
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../http.service";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.scss"],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _http: HttpService) {}
 
   ngOnInit() {
+    this._http.myMethod();
   }
-
 }
